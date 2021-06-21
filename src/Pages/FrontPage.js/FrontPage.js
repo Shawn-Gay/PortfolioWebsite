8165c0e0bf05e68432606201d71121c10e157cff
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-06-20 22:59:23 -0500

    Inital page setup and described theme: glitched

---
+++ src/Pages/FrontPage.js/FrontPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FPstyles.css'
+
+export default function FrontPage() {
+    return (
+        <div className="page-container">
+            <div className="glitch" data-text="FrontPage">FrontPage</div>
+        </div>
+    )
+}
---
+++ src/Pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Mstyles.css'
+
+// Page imports 
+import FrontPage from './FrontPage.js'
+
+
+export default function Main() {
+    return (
+        <div>
+            <section className="front-page">
+                <FrontPage />
+            </section>
+        </div>
+    )
+}
