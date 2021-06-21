a15429952b0608bfa2c07216a5b8aa29e8d71ea9
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-06-21 00:21:09 -0500

    Added an about template page, created transition page divider, todo: make animations on landing page

---
+++ src/Pages/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./APstyles.css"
+
+export default function AboutPage() {
+    return (
+        <div className="about-page-container">
+            AboutPage
+        </div>
+    )
+}
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './Mstyles.css'
 
 // Page imports 
-import FrontPage from './FrontPage.js'
-
+import FrontPage from './FrontPage/'
+import AboutPage from './AboutPage/'
 
 export default function Main() {
     return (
         <div>
             <section className="front-page">
                 <FrontPage />
+            </section>
+            <section>
+                <AboutPage />
             </section>
         </div>
     )
