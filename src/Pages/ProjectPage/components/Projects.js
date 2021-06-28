782e9255e4e3df7cdcce80edd578bd1397d09308
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-06-27 21:50:26 -0500

    redesigned single project cells to smoother animation and better practices

--- src/Pages/ProjectPage/components/Projects.js
+++ src/Pages/ProjectPage/components/Projects.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
+import { SocialIcon } from 'react-social-icons';
+
 export default function Projects() {
     return (
         <section className="project-list-container">
-            <div className="single-project sketchi best-project">
+            <div className="single-project sketchi">
                 <div className="image">
                 <div className="project-links">
-                    <div>Github</div>
-                    <div>Website</div>
+                    <a className="link-tag github" target="_blank" href="https://github.com/Shawn-Gay">
+                    <SocialIcon style={{'width': '30px', 'height': '30px'}} bgColor='black' network="github" />
+                    Github 
+                    </a>
+                    <a className="link-tag website" target="_blank" href="https://sketchi-draw.herokuapp.com/">
+                    Website
+                    <img src="sketchipng.png" />
+                    </a>
                 </div>
                 </div>
-                <div className="project-title">Project One</div>
             </div>
             <div className="single-project">Project Two</div>
             <div className="single-project">Project Three</div>
+            
         </section>
     )
 }
