0a2e7049a72fef92a350a34e091aa30a114173e7
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-06-28 15:55:32 -0500

    SkillsBox.js now has lots of important libaries inside as physical draggable icons. Will add list to describe all skills below as a expandable.

--- src/Pages/SkillsPage/components/SkillsBox.js
+++ src/Pages/SkillsPage/components/SkillsBox.js
@@ -1,86 +1,223 @@
-import React, { useEffect, useRef } from 'react'
-import Matter from 'matter-js'
+import React, {useEffect, useRef} from "react";
+import Matter, { Body } from "matter-js";
 
-export default function SkillsBox() {
-    const boxRef = useRef(null)
-    const canvasRef = useRef(null)
+const SkillsBox = () => {
+  
+  const scene = useRef(null)
 
-    useEffect(() => {
-        let Engine = Matter.Engine
-        let Render = Matter.Render
-        let World = Matter.World
-        let Bodies = Matter.Bodies
-        let Mouse = Matter.Mouse
-        let MouseConstraint = Matter.MouseConstraint
-        let engine = Engine.create({})
-        let render = Render.create({
-            element: boxRef.current,
-            engine: engine,
-            canvas: canvasRef.current,
-            options: {
-                width: 300,
-                height: 300,
-                background: 'rgb(16, 16, 16)',
-                wireframes: false,
-            },
-        })
-        
-        const walls = [Bodies.rectangle(0, 150, 20, 300, {
-            isStatic: true,
-            render: {
-              fillStyle: 'rgb(64, 64, 64)',
-            },
-          }), Bodies.rectangle(150, 300, 300, 20, {
-            isStatic: true,
-            render: {
-              fillStyle: 'rgb(64, 64, 64)',
-            },
-          }),
-          Bodies.rectangle(300, 150, 20, 300, {
-            isStatic: true,
-            render: {
-              fillStyle: 'rgb(64, 64, 64)',
-            },
-          }),
-          Bodies.rectangle(150, 0, 300, 20, {
-            isStatic: true,
-            render: {
-              fillStyle: 'rgb(64, 64, 64)',
-            },
-          })]
-          
-          const ball = Bodies.circle(150, 0, 10, {
-            restitution: 0.9,
-            render: {
-              fillStyle: 'yellow',
-            },
-          })
-          let canvasMouse = Mouse.create(render.canvas)
-          canvasMouse.pixelRatio = window.devicePixelRatio;
-          const options = {
-            mouse: canvasMouse,
-            constraint: {
-                render: {visible: false}
-            }
-          }
-          let mConstraint = MouseConstraint.create(engine, options)
+  useEffect(() => {
+    let Engine = Matter.Engine;
+    let Render = Matter.Render;
+    let World = Matter.World;
+    let Composite = Matter.Composite;
+    let Bodies = Matter.Bodies;
+    let Mouse = Matter.Mouse;
+    let MouseConstraint = Matter.MouseConstraint;
 
-          engine.gravity.y = 0;
+    let engine = Engine.create({});
 
-          World.add(engine.world, [...walls, ball, mConstraint],  )
-          Matter.Runner.run(engine)
-          Render.run(render)
-
-        }, [])
-        return (
-          <div className="physics-box"
-            ref={boxRef}
-            style={{
-              width: 300,
-              height: 300,
-            }}
-          >
-            <canvas ref={canvasRef} />
-          </div>
-        )
+    let render = Render.create({
+      element: scene.current,
+      engine: engine,
+      options: {
+        width: 350,
+        height: 350,
+        wireframes: false,
+        background: 'rgb(32,32,32)',
       }
+    });
+
+    World.add(engine.world, [
+    Bodies.circle(50, 100, 30, {
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .1,
+          yScale: .1,
+          texture: './img/reactLogo.png'
+        }
+      }
+    }),
+    Bodies.rectangle(225, 175, 28, 28, { 
+      restitution: 1,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .1,
+          yScale: .1,
+          texture: './img/GitIcon.png'
+        }
+      }
+    }),
+    Bodies.rectangle(150, 175, 45, 45, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .05,
+          yScale: .05,
+          texture: './img/JavaScript-logo.png'
+        }
+      }
+    }),
+    Bodies.circle(50, 175, 30, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .02,
+          yScale: .02,
+          texture: './img/reduxlogo.png'
+        }
+      }
+    }),
+    Bodies.circle(100, 250, 25, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .02,
+          yScale: .02,
+          texture: './img/socketio.png'
+        }
+      }
+    }),
+    Bodies.rectangle(250, 250, 45, 50, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .4,
+          yScale: .4,
+          texture: './img/firebase.png'
+        }
+      }
+    }),
+    Bodies.rectangle(175, 250, 45, 50, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .25,
+          yScale: .25,
+          texture: './img/sql.png'
+        }
+      }
+    }),
+    Bodies.circle(125, 100, 25, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .2,
+          yScale: .2,
+          texture: './img/nodejs.png'
+        }
+      }
+    }),
+    Bodies.rectangle(200, 100, 50, 20, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .02,
+          yScale: .02,
+          texture: './img/expressjs.png'
+        }
+      }
+    }),
+    Bodies.rectangle(225, 100, 45, 20, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .015,
+          yScale: .015,
+          texture: './img/stripe.png'
+        }
+      }
+    }),
+    Bodies.rectangle(50, 25, 45, 20, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .2,
+          yScale: .2,
+          texture: './img/moment.png'
+        }
+      }
+    }),
+    Bodies.circle(50, 100, 25, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .1,
+          yScale: .1,
+          texture: './img/html.png'
+        }
+      }
+    }),
+    Bodies.circle(50, 100, 25, { 
+      restitution: .75,
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          xScale: .05,
+          yScale: .05,
+          texture: './img/css.png'
+        }
+      }
+    }),
+  ]);
+
+
+    World.add(engine.world, [
+      Bodies.rectangle(175, -20, 350, 50, { isStatic: true, render:{fillStyle: 'rgb(64,64,64)'} }),
+      Bodies.rectangle(175, 370, 350, 50, { isStatic: true, render:{fillStyle: 'rgb(64,64,64)'} }),
+      Bodies.rectangle(370, 175, 50, 350, { isStatic: true, render:{fillStyle: 'rgb(64,64,64)'} }),
+      Bodies.rectangle(-20, 175, 50, 350, { isStatic: true, render:{fillStyle: 'rgb(64,64,64)'} }),
+    ]);
+
+    
+    const push = function(engine){
+      const bodies = Composite.allBodies(engine.world);
+        for (let body of bodies) {
+          if(!body.isStatic){
+            console.log(body)
+            let forceMagnitude = 0.05 * body.mass;
+            Body.applyForce(body, body.position, {
+              x: forceMagnitude * .05, 
+              y: -forceMagnitude + .5 * .05
+            });
+          }
+        }
+    }
+    push(engine)
+
+    let mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          render: {
+            visible: false
+          }
+        }
+      });
+
+    World.add(engine.world, mouseConstraint);
+
+    engine.gravity.y = 0;
+
+    console.log(engine)
+
+    Matter.Runner.run(engine);
+
+    Render.run(render);
+  }, [])
+
+    return <div ref={scene} />;
+}
+export default SkillsBox;
--- src/Pages/SkillsPage/index.js
+++ src/Pages/SkillsPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./SPstyles.css"
 
-import SkillsBox from './components/SkillsBox'
+import SkillsBox from './components/SkillsBox.js'
 
 export default function SkillsPage() {
     return (
