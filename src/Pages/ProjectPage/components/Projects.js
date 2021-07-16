e728c20063225c9fd3d2983e425446bcd66af92b
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-07-16 12:33:13 -0500

    Removed project two/three templates

--- src/Pages/ProjectPage/components/Projects.js
+++ src/Pages/ProjectPage/components/Projects.js
@@ -5,9 +5,8 @@ export default function Projects() {
     return (
         <section className="project-list-container">
             <Sketchi />
-            <div className="single-project">Project Two</div>
-            <div className="single-project">Project Three</div>
-            
+            {/* <div className="single-project">Project Two</div>
+            <div className="single-project">Project Three</div> */}
         </section>
     )
 }
